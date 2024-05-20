dbe8a4d4794f71862531ee88168a3aa665330d51
nest3r <ernestandreev1@gmai.com>
2024-05-20 16:46:40 +0300

    Done

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Game {self.title}>'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if title and year:
+            new_game = Game(title=title, year=int(year))
+            db.session.add(new_game)
+            db.session.commit()
+        return redirect(url_for('index'))
+    games = Game.query.all()
+    return render_template('index.html', games=games)
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    db.session.query(Game).delete()
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Project {self.title}>'
+
