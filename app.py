33dd9e9471ea0b0b998578cc39a23681cdbe9185
nest3r <ernestandreev1@gmai.com>
2024-06-12 22:07:35 +0300

    done2

--- app.py
+++ app.py
@@ -2,38 +2,40 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Game(db.Model):
+# Модель базы данных
+class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
+    link = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f'<Game {self.title}>'
+# Создание таблиц
+with app.app_context():
+    db.create_all()
 
-@app.route('/', methods=['GET', 'POST'])
+# Маршруты
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
-        if title and year:
-            new_game = Game(title=title, year=int(year))
-            db.session.add(new_game)
-            db.session.commit()
-        return redirect(url_for('index'))
-    games = Game.query.all()
-    return render_template('index.html', games=games)
+    projects = Project.query.all()
+    return render_template('index.html', projects=projects)
 
-@app.route('/clear', methods=['POST'])
-def clear():
-    db.session.query(Game).delete()
+@app.route('/add', methods=['POST'])
+def add_project():
+    title = request.form['title']
+    link = request.form['link']
+    new_project = Project(title=title, link=link)
+    db.session.add(new_project)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/clear')
+def clear_projects():
+    db.session.query(Project).delete()
     db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
